4c4c24d9043afb654993f0c00670ef7a3ae2c911
Rubén Escabia <ruben.escabia@beeva.com>
2016-04-04 22:56:29 +0200

    Hello World to EndPoints

---
+++ helloEndPoints/src/com/escabia/helloendpoints/HelloEndPointsServlet.java
@@ -0,0 +1,12 @@
+package com.escabia.helloendpoints;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HelloEndPointsServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
