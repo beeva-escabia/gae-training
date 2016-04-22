d4f47644440425ed4af9426f049822ba21198527
Rubén Escabia <ruben.escabia@beeva.com>
2016-04-22 15:55:16 +0200

    Users and google accounts

--- clock/src/com/geatraining/clock/ClockServlet.java
+++ clock/src/com/geatraining/clock/ClockServlet.java
@@ -6,16 +6,34 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.SimpleTimeZone;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class ClockServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSSSSS");
 		fmt.setTimeZone(new SimpleTimeZone(0, ""));
 		
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		String loginUrl = userService.createLoginURL("/");
+		String logoutUrl = userService.createLogoutURL("/");
+		
+		req.setAttribute("user", user);
+		req.setAttribute("loginUrl", loginUrl);
+		req.setAttribute("logoutUrl", logoutUrl);
+		req.setAttribute("currentTime", fmt.format(new Date()));
+		
+		
 		resp.setContentType("text/html");
-		PrintWriter out = resp.getWriter();
-		out.println("<p>The time is: " + fmt.format(new Date()) + "</p>");		
+		
+		RequestDispatcher jsp = req.getRequestDispatcher("/WEB-INF/home.jsp");
+		jsp.forward(req, resp);
 	}
 }
